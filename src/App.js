806ff5c87fb38616b9562868a2c9a92aa498de80
Dhruv Rohatgi <dhruvrohatgi53@gmail.com>
2021-12-23 16:51:02 +0000

    brainColor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 // import Test1 from "./Test1";
 // import Test2 from "./Test2";
 import Test3 from "./Test3";
-import Brain from "./Test4";
+// import Brain from "./Test4";
 import Test5 from "./Test5";
 import Test6 from "./Test6";
 
