782096ba8053f34b584f307b8b7fab4850f460d0
Dhruv Rohatgi <dhruvrohatgi53@gmail.com>
2021-12-25 05:28:44 +0000

    scroll

--- src/App.js
+++ src/App.js
@@ -8,17 +8,18 @@ import Test6 from "./Test6";
 import Bon from "./assets/bon.mp3";
 import Test7 from "./Test7";
 import ReactAudioPlayer from "react-audio-player";
+import Test8Scroll from "./Test8Scroll";
 import "./style.css";
 function App() {
   return (
     <>
-      <ReactAudioPlayer src={Bon} autoPlay={true} controls={false} />
+      {/* <ReactAudioPlayer src={Bon} autoPlay={true} controls={false} /> */}
       {/* <Test1 />
       <Test2 /> */}
       {/* <Brain /> */}
-      <Test7 />
-      <Test6 />
-
+      {/* <Test7 />
+      <Test6 /> */}
+      <Test8Scroll />
       {/* <Test5 />
       <Test3 /> */}
     </>
