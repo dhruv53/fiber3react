d70ea7da84f8fc8c008d32d7ba567d2031d675c1
Dhruv Rohatgi <dhruvrohatgi53@gmail.com>
2021-12-25 15:10:51 +0000

    sequence3

--- src/Test8Scroll.js
+++ src/Test8Scroll.js
@@ -5,15 +5,11 @@ function Test8Scroll() {
   const call = () => {
     const html = document.documentElement;
     const canvas = document.getElementById("hero-lightpass");
-    // const canvasText = document.getElementById("text");
-
     const context = canvas.getContext("2d");
-    // const txtContext = canvasText.getContext("2d");
 
-    const frameCount = 120;
-
+    const frameCount = 60;
     const currentFrame = (index) =>
-      `./assets/sequence2/${index.toString().padStart(4, "0")}.png`;
+      `./assets/sequence3/${index.toString().padStart(4, "0")}.png`;
 
     const preloadImages = () => {
       for (let i = 1; i < frameCount; i++) {
@@ -26,18 +22,19 @@ function Test8Scroll() {
     img.src = currentFrame(1);
     canvas.width = 1080;
     canvas.height = 1080;
-
     img.onload = function () {
       context.drawImage(img, 0, 0);
     };
 
     const updateImage = (index) => {
       img.src = currentFrame(index);
+      console.log(img.src);
       context.drawImage(img, 0, 0);
     };
 
     window.addEventListener("scroll", () => {
-      console.log("Scrolled!");
+      const scrolled = window.scrollY;
+      console.log(scrolled);
       const scrollTop = html.scrollTop;
       const maxScrollTop = html.scrollHeight - window.innerHeight;
       const scrollFraction = scrollTop / maxScrollTop;
@@ -45,8 +42,37 @@ function Test8Scroll() {
         frameCount - 1,
         Math.ceil(scrollFraction * frameCount)
       );
-
       requestAnimationFrame(() => updateImage(frameIndex + 1));
+
+      if (scrolled >= 0 && scrolled <= 250) {
+        document.getElementById("one").style.color = "rgba(255, 255, 255, 1)";
+      } else {
+        document.getElementById("one").style.color = "rgba(255, 255, 255, 0)";
+      }
+
+      if (scrolled >= 2300 && scrolled <= 2600) {
+        document.getElementById("two").style.color = "rgba(255, 255, 255, 1)";
+      } else {
+        document.getElementById("two").style.color = "rgba(255, 255, 255, 0)";
+      }
+
+      if (scrolled >= 3000 && scrolled <= 3300) {
+        document.getElementById("three").style.color = "rgba(255, 255, 255, 1)";
+      } else {
+        document.getElementById("three").style.color = "rgba(255, 255, 255, 0)";
+      }
+
+      if (scrolled >= 3700 && scrolled <= 4000) {
+        document.getElementById("four").style.color = "rgba(255, 255, 255, 1)";
+      } else {
+        document.getElementById("four").style.color = "rgba(255, 255, 255, 0)";
+      }
+
+      // if (scrolled >= 3400 && scrolled <= 3700) {
+      //   document.getElementById("five").style.color = "rgba(255, 255, 255, 1)";
+      // } else {
+      //   document.getElementById("five").style.color = "rgba(255, 255, 255, 0)";
+      // }
     });
 
     preloadImages();
@@ -59,7 +85,29 @@ function Test8Scroll() {
   return (
     <>
       <canvas id="hero-lightpass" />
-      <canvas id="text" />
+      <div className="block">
+        <p id="one">bA3E</p>
+      </div>
+      <div className="block" />
+      <div className="block" />
+      <div className="block">
+        <p className="features" id="two">
+          Know when and where your brain works best
+        </p>
+      </div>
+      <div className="block">
+        <p className="features" id="three">
+          Optimize your time by working on what matters the most
+        </p>
+      </div>
+      <div className="block">
+        <p className="features" id="four">
+          Discover your focus patterns through our trained algorithms
+        </p>
+      </div>
+      {/* <div className="block">
+        <p id="five">Get instant feedback on your concentration shift</p>
+      </div> */}
     </>
   );
 }
