512a47817b0adcec59c47506fc498ad70940e6cf
Dhruv Rohatgi <dhruvrohatgi53@gmail.com>
2021-12-25 05:31:15 +0000

    scroll

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import React from "react";
 // import Test3 from "./Test3";
 // import Brain from "./Test4";
 // import Test5 from "./Test5";
-import Test6 from "./Test6";
-import Bon from "./assets/bon.mp3";
-import Test7 from "./Test7";
-import ReactAudioPlayer from "react-audio-player";
+// import Test6 from "./Test6";
+// import Bon from "./assets/bon.mp3";
+// import Test7 from "./Test7";
+// import ReactAudioPlayer from "react-audio-player";
 import Test8Scroll from "./Test8Scroll";
 import "./style.css";
 function App() {
